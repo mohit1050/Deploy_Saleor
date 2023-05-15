7d8cc95d528017687eb6c0611a8d55c3a8b50303
Mohit Mahajan <52375687+mohit1050@users.noreply.github.com>
2023-05-15 14:27:12 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -171,7 +171,7 @@ case "$OS" in
         Ubuntu)
                 sudo apt-get update
 		sudo add-apt-repository ppa:deadsnakes/ppa -y
-                sudo apt-get install -y build-essential python3.9 python3-dev python3.9pip python3.9-cffi python3.9-venv gcc
+                sudo apt-get install -y build-essential python3.9 python3-dev python3pip python3-cffi python3.9-venv gcc
                 sudo apt-get install -y libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
 		
                 sudo apt-get install -y postgresql postgresql-contrib
