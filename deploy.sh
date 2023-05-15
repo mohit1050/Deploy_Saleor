7730a56dcf17aeb1791b957d375d0ae0cd51ed06
Mohit Mahajan <52375687+mohit1050@users.noreply.github.com>
2023-05-15 16:26:24 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -154,7 +154,7 @@ sleep 1
 case "$OS" in
         Debian)
                 sudo apt-get update
-                sudo apt-get install -y build-essential python3.9-dev python3-pip python3-cffi python3-venv gcc
+                sudo apt-get install -y build-essential python3-dev python3-pip python3-cffi python3-venv gcc
                 sudo apt-get install -y libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
                 sudo apt-get install -y nodejs npm postgresql postgresql-contrib
                 ;;
