57efc26123bf82580e723bfe7caddda72643ef29
Mohit Mahajan <52375687+mohit1050@users.noreply.github.com>
2023-05-15 14:25:23 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -175,6 +175,8 @@ case "$OS" in
                 sudo apt-get install -y libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
 		
                 sudo apt-get install -y postgresql postgresql-contrib
+		sudo apt install nginx
+		sudo apt-get install libxml2-dev libxslt-dev
 		
 		# Install nvm (if not already installed)
 		if ! command -v nvm &> /dev/null; then
