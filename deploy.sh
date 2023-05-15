b18cb1131ee06c6215ab647907e86a59ca148820
Mohit Mahajan <52375687+mohit1050@users.noreply.github.com>
2023-05-15 16:38:56 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -173,8 +173,8 @@ case "$OS" in
                 sudo apt-get install -y build-essential python3-dev python3-pip python3-cffi python3-venv gcc
                 sudo apt-get install -y libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
                 sudo apt-get install -y postgresql postgresql-contrib
-		sudo apt install nginx
-		sudo apt-get install libxml2-dev libxslt-dev
+		sudo apt install nginx -y
+		sudo apt-get install libxml2-dev libxslt-dev -y
 		# Install nvm (if not already installed)
 		if ! command -v nvm &> /dev/null; then
 		  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
